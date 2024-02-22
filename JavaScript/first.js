64e9483490453285c8bcad9c9ed26f655f264a33
Rana Harshiv <ranaharshiv01@gmail.com>
2024-02-22 20:52:52 +0530

    done the loops and the strings

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -57,3 +57,64 @@ const cricketer = {
 };
 
 console.log(cricketer.age);
+
+// loops 
+
+// for(let i=1;i<=10;i++){
+//     console.log("Hii Harshiv!");
+// }
+
+// while loop 
+// let i = 1;
+// while(i<=10){
+//     console.log("Hii Harshiv!");
+// }
+
+// do while loop
+// let i = 1; 
+// do{
+//      console.log("Hii Harshiv!");
+//     i++;
+// }while(i<=5);
+
+
+// for of loop
+// let str = "harshiv";
+// let length = 0; 
+// for(let i of str){
+//     console.log(i);
+//     length++;
+// }
+// console.log("size is: ",length);
+
+// for in loop 
+// let player = {
+
+//     name: "kohli",
+//     age : 24,
+//     pos : "bat",
+
+// };
+
+// for (let key in player){
+//     console.log(key,"= ",player[key]);
+// }
+
+// print all the even numbers between 1 to 100
+// for(let i=0;i<=100;i++){
+    
+//     if(i%2==0){
+//         console.log(i);
+//     }
+    
+// }
+
+
+// guessing number game 
+let guessnum = 20;
+let res = prompt("Enter the guessing number ");
+
+while(res != guessnum){
+    res = prompt("You enter the wrong number ! enter again");
+}
+console.log("congratuation you enter the right number !");
