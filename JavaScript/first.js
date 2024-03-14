9dab533cd6704e15f09acc47bfad26d6c1c37f62
Rana Harshiv <ranaharshiv01@gmail.com>
2024-03-14 23:21:56 +0530

    javascript

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,62 +1,62 @@
-console.log("hello harshiv !");
-console.log("this is rana here!");
-console.log("here you learning the js");
+// console.log("hello harshiv !");
+// console.log("this is rana here!");
+// console.log("here you learning the js");
 
 // variables
 // a and A both are different
 // "$" " _"  we can start the variable name 
 
-name = "raaaaa";
-console.log(name);
-x = null;
-console.log(x);
-y = undefined;
-console.log(y);
-a = true;
-console.log(a);
+// name = "raaaaa";
+// console.log(name);
+// x = null;
+// console.log(x);
+// y = undefined;
+// console.log(y);
+// a = true;
+// console.log(a);
 
 // var let const 
 
-var name = "rana"
-console.log(name);
-name = "harshiv";
-console.log(name);  // can be change
-var name = "patel";
-console.log(name);  // can be redeclared
+// var name = "rana"
+// console.log(name);
+// name = "harshiv";
+// console.log(name);  // can be change
+// var name = "patel";
+// console.log(name);  // can be redeclared
 
 
-let age = 20;
-console.log(age);
-age = 26;
-console.log(age);  // can be change but can't be re declared agian
+// let age = 20;
+// console.log(age);
+// age = 26;
+// console.log(age);  // can be change but can't be re declared agian
 
-const study = "mca";
-console.log(study);  // can't be change or cant't be re declared agian
+// const study = "mca";
+// console.log(study);  // can't be change or cant't be re declared agian
 
 // it is better practise to use the let for declare the variable in js 
 
 
 // scope wise declarations
 
-{
-    let age = 20;
-    console.log(age);
-}
+// {
+//     let age = 20;
+//     console.log(age);
+// }
 
-{
-    let age = 30;
-    console.log(age);
-    typeof age;
-}
+// {
+//     let age = 30;
+//     console.log(age);
+//     typeof age;
+// }
 
-const cricketer = {
-    name:"dhoni",
-    age : 45,
-    role:"wk-batsman",
+// const cricketer = {
+//     name:"dhoni",
+//     age : 45,
+//     role:"wk-batsman",
 
-};
+// };
 
-console.log(cricketer.age);
+// console.log(cricketer.age);
 
 // loops 
 
@@ -225,10 +225,16 @@ console.log(cricketer.age);
 
 // h2.innerText = h2.innerText + " harshiv here ";
 
-let divs = document.querySelectorAll(".box");
-divs[0].innerText = "new value to div 1";
-divs[1].innerText = "something unique to div 2";
-divs[2].innerText = "yeahhh finalyy we understood";
-console.log(divs);
+// let divs = document.querySelectorAll(".box");
+// divs[0].innerText = "new value to div 1";
+// divs[1].innerText = "something unique to div 2";
+// divs[2].innerText = "yeahhh finalyy we understood";
+// console.log(divs);
+
+
+document.getElementById('demo1').innerHTML = Date();
+
+document.getElementById('b1').onclick= document.getElementById('demo1').innerHTML = Date();
+// document.getElementById('demo1').innerHTML = alert('hello');
 
 
